9748bece4cbf569e314d42a51c9872bdc3e6d6ac
elchinhumbatov <elchinhumbatov@yahoo.com>
2021-08-17 22:06:43 +0400

    gh-pages

--- src/components/Header/middle/Login.jsx
+++ src/components/Header/middle/Login.jsx
@@ -1,49 +1,118 @@
-import React, {useState} from 'react'
-import s from './Login.module.css'
+import React, { useState } from "react";
+import { Formik } from "formik";
+import s from "./Login.module.css";
 
-function Login({toggleLogin}) {
-  const [form, setForm] = useState('login');
+function Login({ toggleLogin }) {
+  const [form, setForm] = useState("login");
 
   const handleForm = (data) => {
-    setForm(data)
-  }
+    setForm(data);
+  };
   const handleToggleLogin = (e) => {
-    e.target.getAttribute("data-target") === 'out' && toggleLogin();
-  }
+    e.target.getAttribute("data-target") === "out" && toggleLogin();
+  };
 
   return (
-    <div data-target='out' className={s.pageWrap} onClick={handleToggleLogin}>
+    <div data-target="out" className={s.pageWrap} onClick={handleToggleLogin}>
       <div className={s.formWrap}>
-        { form === 'login' ? <LoginForm handleForm={handleForm} /> : <RegistrForm handleForm={handleForm} /> }
+        {form === "login" ? (
+          <LoginForm handleForm={handleForm} />
+        ) : (
+          <RegistrForm handleForm={handleForm} />
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-function LoginForm({handleForm}) {
+function LoginForm({ handleForm }) {
   return (
     <div>
       <div className={s.title}>
         <p>Вход</p>
-        <p onClick={()=>{handleForm('registr')}}>Регистрация</p>
+        <p
+          onClick={() => {
+            handleForm("registr");
+          }}
+        >
+          Регистрация
+        </p>
       </div>
       <div className={s.loginForm}>
-      login bla bla
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {errors.email && touched.email && errors.email}
+              <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              {errors.password && touched.password && errors.password}
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
-  )
+  );
 }
 
-function RegistrForm({handleForm}) {
+function RegistrForm({ handleForm }) {
   return (
     <div>
       <div className={s.title}>
         <p>Регистрация</p>
-        <p onClick={()=>{handleForm('login')}}>Вход</p>
+        <p
+          onClick={() => {
+            handleForm("login");
+          }}
+        >
+          Вход
+        </p>
       </div>
       <p>registr registr </p>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
